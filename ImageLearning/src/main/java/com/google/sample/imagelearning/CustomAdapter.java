5f0ba7b91e713121539fc2ea221ae319fd90eabb
readbeard <matteo.principe92@gmail.com>
2017-02-08 19:38:14 +0100

    Couple of graphical changes to dialog

--- ImageLearning/src/main/java/com/google/sample/imagelearning/CustomAdapter.java
+++ ImageLearning/src/main/java/com/google/sample/imagelearning/CustomAdapter.java
@@ -93,7 +93,7 @@ public class CustomAdapter  extends BaseAdapter{
 
         if(selectedIndex!= -1 && position == selectedIndex)
         {
-            holder.tv.setBackgroundColor(ContextCompat.getColor(vi.getContext(),R.color.colorAccent));
+            holder.tv.setBackgroundColor(ContextCompat.getColor(vi.getContext(),R.color.blue));
         }
         else
         {
